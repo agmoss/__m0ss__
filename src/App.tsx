90542f3a9e85b2c2d8ca532ca2fb65edac494237
Andrew Moss <andrew@m0ss.dev>
2020-01-12 12:58:48 -0700

    reverting things back

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Landing from "./containers/landing";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Landing from "./pages/landing";
 import { Circles } from "./pages/circles";
 import Readme from "./pages/readme";
 import withFade from "./components/withFade";
-
-import { CSSTransition } from "react-transition-group";
-
-import "./styles.css";
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-const routes = [
-    { path: "/", name: "Landing", Component: Landing },
-    { path: "/README", name: "Readme", Component: Readme },
-    { path: "/circles", name: "Circles", Component: Circles },
-];
 
 const App: React.FC = () => {
     return (
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+// MaterialUI
 import { createStyles, makeStyles, Grid } from "@material-ui/core";
 
+// Components
 import withTheme from "../components/withTheme";
 
+// Views
 import { Footer } from "../views/footerPrimary";
 import { Header } from "../views/header";
 import { Hero } from "../views/heroPrimary";
 import { MainContent } from "../views/mainContent";
 
-interface ILandingProps {
-    img: any;
-    md: any;
-}
+import ReactLoading from "react-loading";
 
-const Landing = ({ img, md }: ILandingProps) => {
+const Landing = () => {
     const useStyles = makeStyles(() =>
         createStyles({
             root: {
@@ -25,17 +25,100 @@ const Landing = ({ img, md }: ILandingProps) => {
         })
     );
 
+    const [md, setMd] = useState<string>("");
+    const [img, setImg] = useState<Blob>(new Blob());
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        enum FileType {
+            Image,
+            Markdown,
+        }
+
+        const getFile = async (url: string, fileType: FileType) => {
+            const response = await fetch(url);
+            switch (fileType) {
+                case FileType.Image:
+                    if (response.ok) {
+                        const data = await response.blob();
+                        const blob = new Blob([data], { type: "image/jpeg" });
+                        return blob;
+                    } else {
+                        throw new Error("error");
+                    }
+                case FileType.Markdown:
+                    if (response.ok) {
+                        const data = await response.blob();
+                        const blob = new Blob([data], { type: "text/plain" });
+                        return blob;
+                    } else {
+                        throw new Error("error");
+                    }
+            }
+        };
+
+        const setText = async () => {
+            try {
+                const f = await getFile(
+                    "https://ggantstorage.blob.core.windows.net/markdown/landing.md",
+                    FileType.Markdown
+                );
+                const text = await new Response(f).text();
+                setMd(text);
+            } catch {
+                setMd("Error");
+            }
+        };
+
+        const setFile = async () => {
+            try {
+                const i = await getFile(
+                    "https://ggantstorage.blob.core.windows.net/images/DSC_7024.JPG",
+                    FileType.Image
+                );
+                setImg(i);
+            } catch {
+                setImg(
+                    await getFile(
+                        "https://source.unsplash.com/random/600x600",
+                        FileType.Image
+                    )
+                );
+            }
+        };
+
+        Promise.all([setText(), setFile()]).then(() => {
+            setLoading(false);
+        });
+    }, []);
+
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-            <React.Fragment>
-                <Header />
-                <Hero img={img} />
-                <MainContent md={md} />
-                <Footer />
-            </React.Fragment>
+            {loading ? (
+                <Grid
+                    container={true}
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minHeight: "100vh" }}
+                >
+                    <Grid item={true}>
+                        <ReactLoading type={"bars"} color={"white"} />
+                    </Grid>
+                </Grid>
+            ) : (
+                <React.Fragment>
+                    <Header />
+                    <Hero img={img} />
+                    <MainContent md={md} />
+                    <Footer />
+                </React.Fragment>
+            )}
         </div>
     );
 };
 
-export default Landing;
+export default withTheme(Landing);
