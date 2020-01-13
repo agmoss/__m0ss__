55caf6482c666d6f58947c020f959fde70e110eb
Andrew Moss <andrew@m0ss.dev>
2020-01-12 19:01:19 -0700

    factoring out common css properties

--- src/views/card.tsx
+++ src/views/card.tsx
@@ -14,39 +14,38 @@ export const Card = ({ primary, secondary }: IProps) => {
         config: { mass: 5, tension: 500, friction: 80 },
     });
 
+    const common: React.CSSProperties = {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        willChange: "transform, opacity",
+        cursor: "pointer",
+        backgroundSize: "cover",
+    };
+
     return (
         <div onClick={() => set(state => !state)}>
             <a.div
                 className="c back"
                 style={{
+                    ...common,
                     opacity: opacity.interpolate(o => {
                         const on = o as number;
                         return 1 - on;
                     }),
                     transform,
                     backgroundImage: `url(${URL.createObjectURL(primary)})`,
-                    position: "absolute",
-                    width: "100%",
-                    height: "100%",
-                    willChange: "transform, opacity",
-                    cursor: "pointer",
-                    backgroundSize: "cover",
                 }}
             />
             <a.div
                 className="c front"
                 style={{
+                    ...common,
                     opacity,
                     transform: transform.interpolate(
                         t => `${t} rotateX(180deg)`
                     ),
                     backgroundImage: `url(${URL.createObjectURL(secondary)})`,
-                    position: "absolute",
-                    width: "100%",
-                    height: "100%",
-                    willChange: "transform, opacity",
-                    cursor: "pointer",
-                    backgroundSize: "cover",
                 }}
             />
         </div>
