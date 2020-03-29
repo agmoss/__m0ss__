d291b00c6c372fbe55fabd9911f9ae6232bbf4e2
Andrew Moss <andrew@m0ss.dev>
2020-03-29 12:21:42 -0600

    color textfield

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -3,10 +3,10 @@ import { TextField, withStyles, Theme } from "@material-ui/core";
 export default withStyles((theme: Theme) => ({
     root: {
         "& label.Mui-focused": {
-            color: theme.palette.secondary.light,
+            color: theme.palette.primary.main,
         },
         "& .MuiInput-underline:after": {
-            borderBottomColor: theme.palette.secondary.light,
+            borderBottomColor: theme.palette.primary.main,
         },
     },
 }))(TextField);
