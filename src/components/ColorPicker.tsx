439a01da9ec2488b8f6a3fb4a92ecb6386d02d8a
Andrew Moss <andrew@m0ss.dev>
2020-05-30 17:00:28 -0600

    adding custom build

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTheme } from "@material-ui/core";
-import { BlockPicker, ColorResult } from "react-color";
+import { CirclePicker, ColorResult } from "react-color";
 
 interface IColorPicker {
     setColor: Function;
@@ -14,30 +14,56 @@ const ColorPicker = ({ setColor }: IColorPicker) => {
     }
 
     const colorSelections = [
-        "#f44336",
-        "#e91e63",
-        "#9c27b0",
-        "#673ab7",
-        "#3f51b5",
-        "#2196f3",
-        "#03a9f4",
-        "#00bcd4",
-        "#009688",
-        "#4caf50",
-        "#8bc34a",
-        "#cddc39",
-        "#ffeb3b",
-        "#ffc107",
-        "#ff9800",
-        "#ff5722",
-        "#795548",
-        "#607d8b",
+        "#FF6633",
+        "#FFB399",
+        "#FF33FF",
+        "#FFFF99",
+        "#00B3E6",
+        "#E6B333",
+        "#3366E6",
+        "#999966",
+        "#99FF99",
+        "#B34D4D",
+        "#80B300",
+        "#809900",
+        "#E6B3B3",
+        "#6680B3",
+        "#66991A",
+        "#FF99E6",
+        "#CCFF1A",
+        "#FF1A66",
+        "#E6331A",
+        "#33FFCC",
+        "#66994D",
+        "#B366CC",
+        "#4D8000",
+        "#B33300",
+        "#CC80CC",
+        "#66664D",
+        "#991AFF",
+        "#E666FF",
+        "#4DB3FF",
+        "#1AB399",
+        "#E666B3",
+        "#33991A",
+        "#CC9999",
+        "#B3B31A",
+        "#00E680",
+        "#4D8066",
+        "#809980",
+        "#E6FF80",
+        "#1AFF33",
+        "#999933",
+        "#FF3380",
+        "#CCCC00",
+        "#66E64D",
+        "#4D80CC",
+        "#9900B3",
     ];
 
     return (
-        <BlockPicker
+        <CirclePicker
             width="100%"
-            triangle="hide"
             colors={colorSelections}
             color={theme.palette.primary.main}
             onChange={onChange}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const APP_PATH = path.resolve(__dirname, "src");
+const PUBLIC_PATH = path.resolve(__dirname, "public");
+
+module.exports = {
+    entry: APP_PATH,
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ["file-loader?name=[name].[ext]"],
+            },
+        ],
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.join(PUBLIC_PATH, "index.html"),
+        }),
+        new CopyPlugin({
+            patterns: [{ from: PUBLIC_PATH, to: "" }],
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+    ],
+};
